d3257fb30bcc911048ae4766adb9b9c253d3b2db
Nishanth Shankar <nishanths@goodbox.in>
2018-04-18 19:46:16 +0530

    Change to suit newer versions

--- style.js
+++ style.js
@@ -1,84 +1,82 @@
-'use strict';
+import { StyleSheet, Dimensions } from 'react-native'
 
-import { StyleSheet, Dimensions } from 'react-native';
+const {height, width} = Dimensions.get('window')
 
-const {height, width} = Dimensions.get('window');
-
-const PADDING = 8;
-const BORDER_RADIUS = 5;
-const FONT_SIZE = 16;
-const HIGHLIGHT_COLOR = 'rgba(0,118,255,0.9)';
-const OPTION_CONTAINER_HEIGHT = 400;
+const PADDING = 8
+const BORDER_RADIUS = 5
+const FONT_SIZE = 16
+const HIGHLIGHT_COLOR = '#858585'
+const OPTION_CONTAINER_HEIGHT = 400
 
 export default StyleSheet.create({
 
-    overlayStyle: {
-        width: width,
-        height: height,
-        backgroundColor: 'rgba(0,0,0,0.7)'
-    },
+  overlayStyle: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
 
-    optionContainer: {
-        borderRadius:BORDER_RADIUS,
-        width:width*0.8,
-        height:OPTION_CONTAINER_HEIGHT,
-        backgroundColor:'rgba(255,255,255,0.8)',
-        left:width*0.1,
-        top:(height-OPTION_CONTAINER_HEIGHT)/2
-    },
+  optionContainer: {
+    borderRadius: BORDER_RADIUS,
+    width: width * 0.85,
+    maxHeight: OPTION_CONTAINER_HEIGHT,
+    backgroundColor: 'rgba(255,255,255,1)',
+    margin: 4
+  },
 
-    cancelContainer: {
-        left:width*0.1,
-        top:(height-OPTION_CONTAINER_HEIGHT)/2 + 10
-    },
+  cancelContainer: {
+    left: width * 0.1,
+    top: (height - OPTION_CONTAINER_HEIGHT) / 2 + 10
+  },
 
-    selectStyle: {
-        flex: 1,
-        borderColor: '#ccc',
-        borderWidth: 1,
-        padding: 8,
-        borderRadius: BORDER_RADIUS
-    },
+  selectStyle: {
+    flex: 1,
+    borderColor: '#e9e9e9',
+    borderWidth: 1,
+    padding: 8,
+    justifyContent: 'center',
+    borderRadius: BORDER_RADIUS
+  },
 
-    selectTextStyle: {
-        textAlign: 'center',
-        color: '#333',
-        fontSize: FONT_SIZE
-    },
+  selectTextStyle: {
+    color: HIGHLIGHT_COLOR,
+    fontSize: FONT_SIZE
+  },
 
-    cancelStyle: {
-        borderRadius: BORDER_RADIUS,
-        width: width * 0.8,
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        padding: PADDING
-    },
+  cancelStyle: {
+    borderRadius: BORDER_RADIUS,
+    width: width * 0.8,
+    backgroundColor: 'rgba(255,255,255,1)',
+    padding: PADDING
+  },
 
-    cancelTextStyle: {
-        textAlign: 'center',
-        color: '#333',
-        fontSize: FONT_SIZE
-    },
+  cancelTextStyle: {
+    textAlign: 'center',
+    color: '#333',
+    fontSize: FONT_SIZE
+  },
 
-    optionStyle: {
-        padding: PADDING,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
-    },
+  optionStyle: {
+    justifyContent: 'center',
+    minHeight: 44,
+    padding: PADDING
+  },
 
-    optionTextStyle: {
-        textAlign: 'center',
-        fontSize: FONT_SIZE,
-        color: HIGHLIGHT_COLOR
-    },
+  optionTextStyle: {
+    lineHeight: FONT_SIZE,
+    fontSize: FONT_SIZE,
+    color: HIGHLIGHT_COLOR
+  },
 
-    sectionStyle: {
-        padding: PADDING * 2,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc'
-    },
+  sectionStyle: {
+    padding: PADDING * 2,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
 
-    sectionTextStyle: {
-        textAlign: 'center',
-        fontSize: FONT_SIZE
-    }
-});
+  sectionTextStyle: {
+    textAlign: 'center',
+    fontSize: FONT_SIZE
+  }
+})
