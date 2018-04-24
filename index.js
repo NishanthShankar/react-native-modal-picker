110d44543cb9bc31e2804d610258aed642260596
Nishanth Shankar <nishanths@goodbox.in>
2018-04-24 07:54:37 +0530

    Reduce prop types

--- index.js
+++ index.js
@@ -16,19 +16,20 @@ import BaseComponent from './BaseComponent'
 
 let componentIndex = 0
 
+// style: View.propTypes.style,
+//   selectStyle: View.propTypes.style,
+//   optionStyle: View.propTypes.style,
+//   optionTextStyle: Text.propTypes.style,
+//   sectionStyle: View.propTypes.style,
+//   sectionTextStyle: Text.propTypes.style,
+//   cancelStyle: View.propTypes.style,
+//   cancelTextStyle: Text.propTypes.style,
+//   overlayStyle: View.propTypes.style,
+
 const propTypes = {
   data: PropTypes.array,
   onChange: PropTypes.func,
   initValue: PropTypes.string,
-  style: View.propTypes.style,
-  selectStyle: View.propTypes.style,
-  optionStyle: View.propTypes.style,
-  optionTextStyle: Text.propTypes.style,
-  sectionStyle: View.propTypes.style,
-  sectionTextStyle: Text.propTypes.style,
-  cancelStyle: View.propTypes.style,
-  cancelTextStyle: Text.propTypes.style,
-  overlayStyle: View.propTypes.style,
   cancelText: PropTypes.string
 }
 
